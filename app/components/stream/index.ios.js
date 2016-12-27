e28baa1d7a6f004514a54178f070d67d267bfd9d
crellison <cole.ellison@gmail.com>
2016-12-27 13:43:56 -0500

    initial app scaffolding more complete

---
+++ app/components/stream/index.ios.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  Text,
+  View
+} from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+const styles = require('../styles');
+
+module.exports = React.createClass({
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text style={styles.welcome}>
+          Welcome to StreamChain!
+        </Text>
+        <Text style={styles.instructions}>
+          To get started, edit index.ios.js
+        </Text>
+        <Text style={styles.instructions}>
+          Press Cmd+R to reload,{'\n'}
+          Cmd+D or shake for dev menu
+        </Text>
+      </View>
+    );
+  }
+})
